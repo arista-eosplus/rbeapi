8a891e8b17f2679a727208fe7f915a2c2cab55f7
Peter Sprygada <sprygada@arista.com>
2015-02-11 13:44:33 -0500

    updates the ipinterfaces api module
    This updates the ipinterfaces api module with minor fixes and enhancements. It
    also adds the system spec tests for the ipinterface api module

---
+++ lib/rbeapi/api/ipinterfaces.rb
@@ -0,0 +1,179 @@
+#
+# Copyright (c) 2014, Arista Networks, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+#   Neither the name of Arista Networks nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARISTA NETWORKS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+require 'rbeapi/api'
+
+module Rbeapi
+
+  module Api
+    #
+    # The Ipinterface class provides an instance for managing logical
+    # IP interfaces configured using eAPI.
+    class Ipinterfaces < Entity
+
+
+      def get(name)
+        config = get_block("interface #{name}")
+        return nil unless config
+        return nil if /\s{3}switchport$/ =~ config
+
+        response = {}
+
+        mdata = /(?<=^\s{3}ip\saddress\s)(.+)$/.match(config)
+        response['address'] = mdata.nil? ? '' : mdata[1]
+
+        mdata = /(?<=mtu\s)(\d+)$/.match(config)
+        response['mtu'] = mdata.nil? ? '': mdata[1]
+
+        helpers = config.scan(/(?<=\s{3}ip\shelper-address\s).+$/)
+        response['helper_addresses'] = helpers
+
+        response
+      end
+
+      ##
+      # Retrieves all logical IP interfaces from the running-configuration
+      # and returns all instances
+      #
+      # Example:
+      #   {
+      #     "Ethernet1": {
+      #       "address" => "1.2.3.4/5",
+      #       "mtu" => "1500",
+      #       "helper_addresses" => ["5.6.7.8", "9.10.11.12"]
+      #     },
+      #     "Ethernet2": {...}
+      #   }
+      #
+      # @return [Hash] all IP interfaces found in the running-config
+      def getall
+        interfaces = config.scan(/(?<=^interface\s).+/)
+        interfaces.each_with_object({}) do |name, hsh|
+          values = get name
+          hsh[name] = values if values
+        end
+      end
+
+      ##
+      # Create a new logical IP interface in the running-config
+      #
+      # @param [String] name The name of the interface
+      #
+      # @return [Boolean] True if the create succeeds otherwise False
+      def create(name)
+        configure(["interface #{name}", 'no switchport'])
+      end
+
+      ##
+      # Deletes a logical IP interface from the running-config
+      #
+      # @param [String] name The name of the interface
+      #
+      # @return [Boolean] True if the create succeeds otherwise False
+      def delete(name)
+        configure(["interface #{name}", 'no ip address', 'switchport'])
+      end
+
+      ##
+      ## Configures the IP address and mask length for the interface
+      #
+      # @param [String] name The name of the interface to configure
+      # @param [Hash] opts The configuration parameters for the interface
+      # @option opts [string] :value The value to set the address to
+      # @option opts [Boolean] :default The value should be set to default
+      #
+      # @return [Boolean] True if the commands succeed otherwise False
+      def set_address(name, opts = {})
+        value = opts[:value]
+        default = opts[:default] || false
+
+        cmds = ["interface #{name}"]
+        case default
+        when true
+          cmds << 'default ip address'
+        when false
+          cmds << (value.nil? ? 'no ip address' : "ip address #{value}")
+        end
+        configure cmds
+      end
+
+      ##
+      ## Configures the MTU value for the interface
+      #
+      # @param [String] name The name of the interface to configure
+      # @param [Hash] opts The configuration parameters for the interface
+      # @option opts [string] :value The value to set the MTU to
+      # @option opts [Boolean] :default The value should be set to default
+      #
+      # @return [Boolean] True if the commands succeed otherwise False
+      def set_mtu(name, opts = {})
+        value = opts[:value]
+        default = opts[:default] || false
+
+        cmds = ["interface #{name}"]
+        case default
+        when true
+          cmds << 'default mtu'
+        when false
+          cmds << (value.nil? ? 'no mtu' : "mtu #{value}")
+        end
+        configure cmds
+      end
+
+      ##
+      ## Configures ip helper addresses for the interface
+      #
+      # @param [String] name The name of the interface to configure
+      # @param [Hash] opts The configuration parameters for the interface
+      # @param [opts] [Array] :value list of addresses to configure as
+      #   helper address on the specified interface
+      # @option opts [Boolean] :default The value should be set to default
+      def set_helper_addresses(name, opts = {})
+        value = opts[:value]
+        default = opts[:default] || false
+
+        cmds = ["interface #{name}"]
+        case default
+        when true
+          cmds << 'default ip helper-address'
+        when false
+          if value.nil?
+            cmds << 'no ip helper-address'
+          else
+            cmds << 'default ip helper-address'
+            value.each { |addr| cmds << "ip helper-address #{addr}" }
+          end
+        end
+        configure cmds
+      end
+    end
+  end
+end
---
+++ spec/system/api_ipinterfaces_spec.rb
@@ -0,0 +1,96 @@
+require 'spec_helper'
+
+require 'rbeapi/client'
+require 'rbeapi/api/ipinterfaces'
+
+describe Rbeapi::Api::Ipinterfaces do
+  subject { described_class.new(node) }
+
+  let(:config) { Rbeapi::Client::Config.new(filename: get_fixture('dut.conf')) }
+  let(:node) { Rbeapi::Client.connect_to('veos02') }
+
+  describe '#get' do
+
+    let(:entity) do
+      { 'address' => '99.99.99.99/24', 'mtu' => '1500',
+        'helper_addresses' => [] }
+    end
+
+    before { node.config(['default interface Ethernet1', 'interface Ethernet1',
+                         'no switchport', 'ip address 99.99.99.99/24']) }
+
+    it 'returns the ipinterface resource' do
+      expect(subject.get('Ethernet1')).to eq(entity)
+    end
+  end
+
+  describe '#getall' do
+    before { node.config(['default interface Ethernet1', 'interface Ethernet1',
+                         'no switchport', 'ip address 99.99.99.99/24']) }
+
+    it 'returns the ipinterface collection' do
+      expect(subject.getall).to include('Ethernet1')
+    end
+
+    it 'returns a hash collection' do
+      expect(subject.getall).to be_a_kind_of(Hash)
+    end
+ end
+
+  describe '#create' do
+    before { node.config(['interface Ethernet1', 'switchport']) }
+
+    it 'creates a new ipinterface resource' do
+      expect(subject.get('Ethernet1')).to be_nil
+      expect(subject.create('Ethernet1')).to be_truthy
+      expect(subject.get('Ethernet1')).not_to be_nil
+    end
+  end
+
+  describe '#delete' do
+    before { node.config(['interface Ethernet1', 'no switchport',
+                          'ip address 99.99.99.99/24']) }
+
+    it 'deletes a ipinterface resource' do
+      expect(subject.get('Ethernet1')).not_to be_nil
+      expect(subject.delete('Ethernet1')).to be_truthy
+      expect(subject.get('Ethernet1')).to be_nil
+    end
+  end
+
+  describe '#set_address' do
+    before { node.config(['default interface Ethernet1', 'interface Ethernet1',
+                          'no switchport'])  }
+
+    it 'sets the address value' do
+      expect(subject.get('Ethernet1')['address']).to be_empty
+      expect(subject.set_address('Ethernet1', value: '99.99.99.99/24')).to be_truthy
+      expect(subject.get('Ethernet1')['address']).to eq('99.99.99.99/24')
+    end
+  end
+
+  describe '#set_mtu' do
+    before { node.config(['default interface Ethernet1', 'interface Ethernet1',
+                          'no switchport'])  }
+
+    it 'sets the mtu value on the interface' do
+      expect(subject.get('Ethernet1')['mtu']).to eq('1500')
+      expect(subject.set_mtu('Ethernet1', value: '2000')).to be_truthy
+      expect(subject.get('Ethernet1')['mtu']).to eq('2000')
+    end
+  end
+
+  describe '#set_trunk_allowed_vlans' do
+    before { node.config(['default interface Ethernet1', 'interface Ethernet1',
+                          'no switchport', 'ip address 99.99.99.99/24'])  }
+
+    let(:helpers) { %w(99.99.99.98 99.99.99.97) }
+
+    it 'sets the helper addresses on the interface' do
+      expect(subject.get('Ethernet1')['helper_addresses']).to be_empty
+      expect(subject.set_helper_addresses('Ethernet1', value: helpers)).to be_truthy
+      expect(subject.get('Ethernet1')['helper_addresses'].sort).to eq(helpers.sort)
+    end
+  end
+end
+
