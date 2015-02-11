23e2e238cc4caa8fcbe5c7c2998bdd5c17d480f1
Peter Sprygada <sprygada@arista.com>
2015-02-11 13:46:28 -0500

    adds the system spec tests for ethernet interfaces

---
+++ spec/system/api_interfaces_ethernet_spec.rb
@@ -0,0 +1,134 @@
+require 'spec_helper'
+
+require 'rbeapi/client'
+require 'rbeapi/api/interfaces'
+
+describe Rbeapi::Api::Interfaces do
+  subject { described_class.new(node) }
+
+  let(:config) { Rbeapi::Client::Config.new(filename: get_fixture('dut.conf')) }
+  let(:node) { Rbeapi::Client.connect_to('veos02') }
+
+  describe '#get' do
+
+    let(:entity) do
+      { 'name' => 'Ethernet1', 'type' => 'ethernet', 'description' => '',
+        'shutdown' => false, 'sflow' => true, 'flowcontrol_send' => 'off',
+        'flowcontrol_receive' => 'off'}
+    end
+
+    before { node.config(['default interface Ethernet1']) }
+
+    it 'returns the interface resource' do
+      expect(subject.get('Ethernet1')).to eq(entity)
+    end
+  end
+
+  describe '#getall' do
+    before { node.config(['default interface Ethernet1']) }
+
+    it 'returns the interface collection' do
+      expect(subject.getall).to include('Ethernet1')
+    end
+
+    it 'returns a hash collection' do
+      expect(subject.getall).to be_a_kind_of(Hash)
+    end
+ end
+
+  describe '#create' do
+    it 'raises an error on create' do
+      expect { subject.create('Ethernet1') }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#delete' do
+    it 'raises an error on create' do
+      expect { subject.create('Ethernet1') }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#default' do
+    before { node.config(['interface Ethernet1', 'shutdown']) }
+
+    it 'sets Ethernet1 to default' do
+      expect(subject.get('Ethernet1')['shutdown']).to be_truthy
+      expect(subject.default('Ethernet1')).to be_truthy
+      expect(subject.get('Ethernet1')['shutdown']).to be_falsy
+    end
+  end
+
+  describe '#set_description' do
+    it 'sets the description value on the interface' do
+      node.config(['interface Ethernet1', 'no description'])
+      expect(subject.get('Ethernet1')['description']).to be_empty
+      expect(subject.set_description('Ethernet1', value: 'foo bar')).to be_truthy
+      expect(subject.get('Ethernet1')['description']).to eq('foo bar')
+    end
+  end
+
+  describe '#set_shutdown' do
+    it 'sets the shutdown value to true' do
+      node.config(['interface Ethernet1', 'no shutdown'])
+      expect(subject.get('Ethernet1')['shutdown']).to be_falsy
+      expect(subject.set_shutdown('Ethernet1', value: true)).to be_truthy
+      expect(subject.get('Ethernet1')['shutdown']).to be_truthy
+    end
+
+    it 'sets the shutdown value to false' do
+      node.config(['interface Ethernet1', 'shutdown'])
+      expect(subject.get('Ethernet1')['shutdown']).to be_truthy
+      expect(subject.set_shutdown('Ethernet1', value: false)).to be_truthy
+      expect(subject.get('Ethernet1')['shutdown']).to be_falsy
+    end
+  end
+
+  describe '#set_sflow' do
+    it 'sets the sflow value to true' do
+      node.config(['interface Ethernet1', 'no sflow enable'])
+      expect(subject.get('Ethernet1')['sflow']).to be_falsy
+      expect(subject.set_sflow('Ethernet1', value: true)).to be_truthy
+      expect(subject.get('Ethernet1')['sflow']).to be_truthy
+    end
+
+    it 'sets the sflow value to false' do
+      node.config(['interface Ethernet1', 'sflow enable'])
+      expect(subject.get('Ethernet1')['sflow']).to be_truthy
+      expect(subject.set_sflow('Ethernet1', value: false)).to be_truthy
+      expect(subject.get('Ethernet1')['sflow']).to be_falsy
+    end
+  end
+
+  describe '#set_flowcontrol_send' do
+    it 'sets the flowcontrol send value to on' do
+      node.config(['interface Ethernet1', 'flowcontrol send off'])
+      expect(subject.get('Ethernet1')['flowcontrol_send']).to eq('off')
+      expect(subject.set_flowcontrol_send('Ethernet1', value: 'on')).to be_truthy
+      expect(subject.get('Ethernet1')['flowcontrol_send']).to eq('on')
+    end
+
+    it 'sets the flowcontrol send value to off' do
+      node.config(['interface Ethernet1', 'flowcontrol send on'])
+      expect(subject.get('Ethernet1')['flowcontrol_send']).to eq('on')
+      expect(subject.set_flowcontrol_send('Ethernet1', value: 'off')).to be_truthy
+      expect(subject.get('Ethernet1')['flowcontrol_send']).to eq('off')
+    end
+  end
+
+  describe '#set_flowcontrol_receive' do
+    it 'sets the flowcontrol receive value to on' do
+      node.config(['interface Ethernet1', 'flowcontrol receive off '])
+      expect(subject.get('Ethernet1')['flowcontrol_receive']).to eq('off')
+      expect(subject.set_flowcontrol_receive('Ethernet1', value: 'on')).to be_truthy
+      expect(subject.get('Ethernet1')['flowcontrol_receive']).to eq('on')
+    end
+
+    it 'sets the flowcontrol receive value to off' do
+      node.config(['interface Ethernet1', 'flowcontrol receive on'])
+      expect(subject.get('Ethernet1')['flowcontrol_receive']).to eq('on')
+      expect(subject.set_flowcontrol_receive('Ethernet1', value: 'off')).to be_truthy
+      expect(subject.get('Ethernet1')['flowcontrol_receive']).to eq('off')
+    end
+  end
+end
+
