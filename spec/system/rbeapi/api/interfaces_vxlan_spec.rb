e1bac30e517658b08b9c6259688cf38c6c7a502a
Peter Sprygada <sprygada@arista.com>
2015-02-16 07:51:11 -0500

    updates vxlan interface in the api modules
    This update finishes the refactor to symoblize the vxlan interface and
    includes update system spec tests.

--- spec/system/rbeapi/api/interfaces_vxlan_spec.rb
+++ spec/system/rbeapi/api/interfaces_vxlan_spec.rb
@@ -6,15 +6,16 @@ require 'rbeapi/api/interfaces'
 describe Rbeapi::Api::Interfaces do
   subject { described_class.new(node) }
 
-  let(:config) { Rbeapi::Client::Config.new(filename: get_fixture('dut.conf')) }
-  let(:node) { Rbeapi::Client.connect_to('veos02') }
+  let(:node) do
+    Rbeapi::Client.config.read(fixture_file('dut.conf'))
+    Rbeapi::Client.connect_to('dut')
+  end
 
   describe '#get' do
 
     let(:entity) do
-      { 'name' => 'Vxlan1', 'type' => 'vxlan', 'description' => '',
-        'shutdown' => false, 'source_interface' => '',
-        'multicast_group' => ''}
+      { name: 'Vxlan1', type: 'vxlan', description: '', shutdown: false,
+        source_interface: '', multicast_group: ''}
     end
 
     before { node.config(['no interface Vxlan1', 'interface Vxlan1']) }
@@ -49,7 +50,7 @@ describe Rbeapi::Api::Interfaces do
   describe '#delete' do
     before { node.config(['interface Vxlan1']) }
 
-    it 'deletes a switchport resource' do
+    it 'deletes a vxlan interface resource' do
       expect(subject.get('Vxlan1')).not_to be_nil
       expect(subject.delete('Vxlan1')).to be_truthy
       expect(subject.get('Vxlan1')).to be_nil
@@ -60,34 +61,34 @@ describe Rbeapi::Api::Interfaces do
     before { node.config(['interface Vxlan1', 'shutdown']) }
 
     it 'sets Vxlan1 to default' do
-      expect(subject.get('Vxlan1')['shutdown']).to be_truthy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_truthy
       expect(subject.default('Vxlan1')).to be_truthy
-      expect(subject.get('Vxlan1')['shutdown']).to be_falsy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_falsy
     end
   end
 
   describe '#set_description' do
     it 'sets the description value on the interface' do
       node.config(['interface Vxlan1', 'no description'])
-      expect(subject.get('Vxlan1')['description']).to be_empty
+      expect(subject.get('Vxlan1')[:description]).to be_empty
       expect(subject.set_description('Vxlan1', value: 'foo bar')).to be_truthy
-      expect(subject.get('Vxlan1')['description']).to eq('foo bar')
+      expect(subject.get('Vxlan1')[:description]).to eq('foo bar')
     end
   end
 
   describe '#set_shutdown' do
     it 'sets the shutdown value to true' do
       node.config(['interface Vxlan1', 'no shutdown'])
-      expect(subject.get('Vxlan1')['shutdown']).to be_falsy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_falsy
       expect(subject.set_shutdown('Vxlan1', value: true)).to be_truthy
-      expect(subject.get('Vxlan1')['shutdown']).to be_truthy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_truthy
     end
 
     it 'sets the shutdown value to false' do
       node.config(['interface Vxlan1', 'shutdown'])
-      expect(subject.get('Vxlan1')['shutdown']).to be_truthy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_truthy
       expect(subject.set_shutdown('Vxlan1', value: false)).to be_truthy
-      expect(subject.get('Vxlan1')['shutdown']).to be_falsy
+      expect(subject.get('Vxlan1')[:shutdown]).to be_falsy
     end
   end
 
@@ -95,9 +96,9 @@ describe Rbeapi::Api::Interfaces do
     before { node.config(['no interface Vxlan1', 'interface Vxlan1']) }
 
     it 'sets the source interface value on the interface' do
-      expect(subject.get('Vxlan1')['source_interface']).to be_empty
+      expect(subject.get('Vxlan1')[:source_interface]).to be_empty
       expect(subject.set_source_interface('Vxlan1', value: 'Loopback0')).to be_truthy
-      expect(subject.get('Vxlan1')['source_interface']).to eq('Loopback0')
+      expect(subject.get('Vxlan1')[:source_interface]).to eq('Loopback0')
     end
   end
 
@@ -105,9 +106,9 @@ describe Rbeapi::Api::Interfaces do
     before { node.config(['no interface Vxlan1', 'interface Vxlan1']) }
 
     it 'sets the multicast group value on the interface' do
-      expect(subject.get('Vxlan1')['multicast_group']).to be_empty
+      expect(subject.get('Vxlan1')[:multicast_group]).to be_empty
       expect(subject.set_multicast_group('Vxlan1', value: '239.10.10.10')).to be_truthy
-      expect(subject.get('Vxlan1')['multicast_group']).to eq('239.10.10.10')
+      expect(subject.get('Vxlan1')[:multicast_group]).to eq('239.10.10.10')
     end
   end
 end
